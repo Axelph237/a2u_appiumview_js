cca531cc01bc5e732ed6353553d6e3ef98ce70dd
CandyCaneCazoo <58153713+Axelph237@users.noreply.github.com>
2024-06-27 09:55:56 -0700

    Updated page layout

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,20 @@ function App() {
 
 
     return (
-        <div className='component-container'>
-            <h1>Control Appium from Web App</h1>
-            <RequestButtons/>
+        <>
+            <div className='page-row'>
+                <div className='page-column' id='text-column' style={{alignItems: 'center', padding: '50px'}}>
+                    <h1>Control Appium from Web App</h1>
+                </div>
+                <div className='page-column' id='interact-column' style={{width: '50vw',alignItems: 'center', padding: '50px'}}>
+                    <RequestButtons/>
 
-            <DraggableComponent className="appiumBox">
-                <iframe className="appiumFrame" src={"http://" + defaultURL}></iframe>
-            </DraggableComponent>
-        </div>
+                    <DraggableComponent className="appiumBox">
+                        <iframe className="appiumFrame" src={"http://" + defaultURL}></iframe>
+                    </DraggableComponent>
+                </div>
+            </div>
+        </>
     )
 }
 
