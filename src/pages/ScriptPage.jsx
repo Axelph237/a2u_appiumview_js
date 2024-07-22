31660a3304255824ae869d3c3d54f5ca89095896
CandyCaneCazoo <58153713+Axelph237@users.noreply.github.com>
2024-07-22 11:18:55 -0700

    Moved function back to parent file. Reasoning: element is not used across site, only in script page. Leave it privated to script page.

--- src/pages/ScriptPage.jsx
+++ src/pages/ScriptPage.jsx
@@ -3,7 +3,6 @@ import {Component, useState} from "react";
 import PropTypes from "prop-types";
 import axios from "axios";
 import ConsoleView from "../components/ConsoleView.jsx";
-import TestInput from "../components/ScriptInputBox.jsx";
 import fitty from "fitty";
 
 export default class ScriptPage extends Component {
@@ -228,9 +227,62 @@ function TestButton({testDef, onClick, background}) {
         </div>
     )
 }
-
 TestButton.propTypes = {
     testDef: PropTypes.object.isRequired,
     onClick: PropTypes.func.isRequired,
     background: PropTypes.string.isRequired,
+}
+
+function TestInput({inputID, defaultValue}) {
+    const [booleanInput, setBooleanInput] = useState(false)
+
+    let inputElement = (<></>)
+    let inputType = typeof defaultValue
+
+    switch (inputType) {
+        case "string":
+            inputElement = (
+                <input name={inputID} className='test-input-box' type='text' defaultValue={defaultValue}/>
+            )
+            break;
+
+        case "number":
+            inputElement = (
+                <input name={inputID} className='test-input-box' type='number' defaultValue={defaultValue}/>
+            )
+            break;
+
+        // TODO fix checkbox formatting
+        case "boolean":
+            inputElement = (
+                <div className='checkbox-container'>
+                    <input name={inputID} className='test-input-box' type='checkbox' value={String(booleanInput)}/>
+                    <div className='tf-container'>
+                        <div className={`boolean-checkbox ${!booleanInput && 'checked'}`} onClick={() => setBooleanInput(false)}>False</div>
+                        <div className={`boolean-checkbox ${booleanInput && 'checked'}`} onClick={() => setBooleanInput(true)}>True</div>
+                    </div>
+                </div>
+            )
+            break;
+
+        case "object":
+            console.log(defaultValue + " is type Object!")
+            break;
+        default:
+            break;
+    }
+
+    return (
+        <div className='input-container'>
+            <div className='layered' style={{width: '100%'}}>
+                <b style={{justifySelf: 'start'}}>{inputID.toUpperCase()}</b>
+                <p style={{justifySelf: 'end'}}>{inputType.toUpperCase()}</p>
+            </div>
+            {inputElement}
+        </div>
+    )
+}
+TestInput.propTypes = {
+    inputID: PropTypes.string.isRequired,
+    defaultValue: PropTypes.any,
 }
