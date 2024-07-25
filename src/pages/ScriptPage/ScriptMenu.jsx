dd9b35e970d22a49222f64c0a018679ab564416b
CandyCaneCazoo <58153713+Axelph237@users.noreply.github.com>
2024-07-25 12:29:15 -0700

    Added container highlighting for selected container. And added ability to unselect container.

--- src/pages/ScriptPage/ScriptMenu.jsx
+++ src/pages/ScriptPage/ScriptMenu.jsx
@@ -1,15 +1,25 @@
 import ScriptContainer from "./ScriptContainer.jsx";
 import PropTypes from "prop-types";
+import {useState} from "react";
 import './ScriptMenu.css'
 
 export default function ScriptMenu({scripts, setActiveScript}) {
+    const [openContainer, setOpenContainer] = useState(-1);
+
+    const handleClick = (scriptID) => {
+        if (openContainer === scriptID)
+            scriptID = -1
+
+        setActiveScript(scriptID)
+        setOpenContainer(scriptID)
+    }
 
     return (
             <div id='script-menu'>
                 {scripts.map(def => (
                     <ScriptContainer script={def}
-                                     onClick={() => {setActiveScript(def.script_id)}}
-                                     background={'var(--a2u-blue)'}
+                                     onClick={() => {handleClick(def.script_id)}}
+                                     background={openContainer === def.script_id ? 'var(--federal-blue)' : 'var(--a2u-blue)'}
                                      key={def.script_id}
                     />
                 ))}
